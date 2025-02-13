5a2994c5cb3a1918ac694e7bc3b3601524d70e88
mysoyul <vega2k_@naver.com>
2025-02-13 15:18:39 +0900

    modelmapper add

--- src/main/java/com/boot3/myrestapi/MyRestApiApplication.java
+++ src/main/java/com/boot3/myrestapi/MyRestApiApplication.java
@@ -1,7 +1,9 @@
 package com.boot3.myrestapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyRestApiApplication {
@@ -11,4 +13,15 @@ public class MyRestApiApplication {
 		SpringApplication.run(MyRestApiApplication.class, args);
 	}
 
+	/*
+		ModelMapper 객체를 Spring Bean 으로 설정하면 Singleton 객체로 생성이 되므로
+		new ModelMapper() 로 생성하는 방법 보다 Heap 메모리를 절약하는 장점이 있다.
+		@Autowired
+		private ModelMapper modelMapper
+	 */
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper;
+	}
 }
