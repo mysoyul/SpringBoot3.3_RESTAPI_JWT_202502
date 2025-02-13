c318930551d0912880e02024605e656856660de9
mysoyul <vega2k_@naver.com>
2025-02-13 12:34:15 +0900

    first commit

---
+++ src/main/java/com/boot3/myrestapi/MyRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.boot3.myrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRestApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyRestApiApplication.class, args);
+	}
+
+}
